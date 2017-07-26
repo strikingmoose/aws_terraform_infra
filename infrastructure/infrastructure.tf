d90c70e03d9209776686abecbf66ae93d974f5bf
chiwang <cwang55@gmail.com>
2017-07-26 16:55:30 -0600

    Changed gif source in post 32

--- infrastructure/infrastructure.tf
+++ infrastructure/infrastructure.tf
@@ -5,10 +5,11 @@ provider "aws" {
 resource "aws_vpc" "main_vpc" {
     cidr_block = "${var.myCidrBlock}"
     instance_tenancy = "default"
+    enable_dns_hostnames = true
+
     tags {
         Name = "main_vpc"
     }
-    enable_dns_hostnames = true
 }
 
 resource "aws_internet_gateway" "main_vpc_igw" {
@@ -32,19 +33,19 @@ resource "aws_default_route_table" "main_vpc_default_route_table" {
     }
 }
 
-resource "aws_subnet" "main_vpc_emr_subnet" {
+resource "aws_subnet" "main_vpc_subnet" {
     vpc_id = "${aws_vpc.main_vpc.id}"
     cidr_block = "${var.myCidrBlock}"
     map_public_ip_on_launch = true
 
     tags {
-        Name = "main_vpc_emr_subnet"
+        Name = "main_vpc_subnet"
     }
 }
 
-resource "aws_default_network_acl" "main_vpc_emr_nacl" {
+resource "aws_default_network_acl" "main_vpc_nacl" {
     default_network_acl_id = "${aws_vpc.main_vpc.default_network_acl_id}"
-    subnet_ids = ["${aws_subnet.main_vpc_emr_subnet.id}"]
+    subnet_ids = ["${aws_subnet.main_vpc_subnet.id}"]
 
     ingress {
         protocol   = -1
@@ -66,11 +67,11 @@ resource "aws_default_network_acl" "main_vpc_emr_nacl" {
     }
 
     tags {
-        Name = "main_vpc_emr_nacl"
+        Name = "main_vpc_nacl"
     }
 }
 
-resource "aws_default_security_group" "main_vpc_emr_security_group" {
+resource "aws_default_security_group" "main_vpc_security_group" {
     vpc_id = "${aws_vpc.main_vpc.id}"
 
     ingress {
@@ -89,7 +90,7 @@ resource "aws_default_security_group" "main_vpc_emr_security_group" {
     }
 
     tags {
-        Name = "main_vpc_emr_security_group"
+        Name = "main_vpc_security_group"
     }
 }
 
@@ -97,18 +98,21 @@ resource "aws_default_security_group" "main_vpc_emr_security_group" {
 //resource "aws_instance" "test" {
 //    ami           = "ami-c58c1dd3"
 //    instance_type = "t2.medium"
-//    security_groups = ["${aws_default_security_group.main_vpc_emr_security_group.id}"]
-//    subnet_id = "${aws_subnet.main_vpc_emr_subnet.id}"
+//    security_groups = ["${aws_default_security_group.main_vpc_security_group.id}"]
+//    subnet_id = "${aws_subnet.main_vpc_subnet.id}"
 //    associate_public_ip_address = true
 //    key_name = "${var.myKeyPair}"
 //}
 
-// Amazon Linux Deep Learning Ami /////////////////////////////////
-//resource "aws_instance" "test" {
-//    ami           = "ami-c58c1dd3"
-//    instance_type = "t2.medium"
-//    security_groups = ["${aws_default_security_group.main_vpc_emr_security_group.id}"]
-//    subnet_id = "${aws_subnet.main_vpc_emr_subnet.id}"
-//    associate_public_ip_address = true
-//    key_name = "${var.myKeyPair}"
-//}
+resource "aws_spot_instance_request" "aws_deep_learning_custom_spot" {
+    ami           = "ami-4b44745d"
+    spot_price    = "0.20"
+    instance_type = "p2.xlarge"
+    security_groups = ["${aws_default_security_group.main_vpc_security_group.id}"]
+    subnet_id = "${aws_subnet.main_vpc_subnet.id}"
+    key_name = "${var.myKeyPair}"
+
+    tags {
+        Name = "aws_deep_learning_custom_spot"
+    }
+}
